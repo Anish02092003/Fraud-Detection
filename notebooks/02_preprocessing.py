1002474e59cd0a7558c8b0e014308f942d7d4a1d
Pritish Kumar Lenka <pritishlenka11@gmail.com>
2026-01-27 09:05:29 +0530

    Update 02_preprocessing.py

--- notebooks/02_preprocessing.py
+++ notebooks/02_preprocessing.py
@@ -2,25 +2,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# -----------------------------
-# 1. Load dataset
-# -----------------------------
 df = pd.read_csv("data/creditcard.csv")
 
 print("Dataset shape:", df.shape)
 
-# -----------------------------
-# 2. Separate features & target
-# -----------------------------
 X = df.drop("Class", axis=1)
 y = df["Class"]
 
 print("\nTarget distribution:")
 print(y.value_counts(normalize=True) * 100)
 
-# -----------------------------
-# 3. Train-Test Split (STRATIFIED)
-# -----------------------------
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -35,9 +26,6 @@ print(y_train.value_counts(normalize=True) * 100)
 print("\nTest target distribution:")
 print(y_test.value_counts(normalize=True) * 100)
 
-# -----------------------------
-# 4. Feature Scaling (ONLY Time & Amount)
-# -----------------------------
 scaler = StandardScaler()
 
 X_train_scaled = X_train.copy()
@@ -51,11 +39,9 @@ X_test_scaled[["Time", "Amount"]] = scaler.transform(
     X_test[["Time", "Amount"]]
 )
 
-# -----------------------------
-# 5. Sanity Check
-# -----------------------------
 print("\nScaled feature statistics (TRAIN):")
 print("Mean:\n", X_train_scaled[["Time", "Amount"]].mean())
 print("\nStd Dev:\n", X_train_scaled[["Time", "Amount"]].std())
 
 print("\nPreprocessing completed successfully ✅")
+
